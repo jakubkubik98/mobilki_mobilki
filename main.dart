e9793a64da5807049cf4149d920254bbefbf3da2
Jakub 'MrHishprung' Kubik <mail.mrhishprung@gmail.com>
2024-01-23 22:37:19 +0100

    initial creation of DB code, waiting for commented code

--- main.dart
+++ main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'initial_screen.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-void main() {
+Future<void> main() async {
+  var databasePath = await getDatabasesPath();
+  String path = join(databasePath, 'database.db');
+  Database database = await openDatabase(path, version: 1,
+    onCreate: (Database db, int (db, version) async {
+      await db.execute('CREATE TABLE ToDoList (id INTEGER PRIMARY KEY, name VARCHAR(255), desc VARCHAR(255)), createdAt TIMESTAMP, isDone BOOL)')
+  }
+  )
+  );
   runApp(const MainApp());
 }
 

c34f681d68f2b9d2ea494117f9bae1cfbde3c6fb
Jakub 'MrHishprung' Kubik <mail.mrhishprung@gmail.com>
2024-01-24 17:09:07 +0100

    Extended ToDo class by additional variables, initially modified getAllToDos() - fix

--- todo.dart
+++ todo.dart
@@ -1,9 +1,15 @@
 class ToDo {
+
   ToDo({
+    required this.id,
     required this.name,
     required this.description,
+    required this.createdAt,
+    required this.isDone,
   });
-
+  final int id;
   final String name;
   final String description;
+  final int createdAt;
+  final bool isDone;
 }
--- todo_repository.dart
+++ todo_repository.dart
@@ -3,11 +3,13 @@ import 'todo.dart';
 class ToDoRepository {
   // Returns a sample list of ToDos
   static List<ToDo> getAllToDos() {
-    // Connection to dabase should be done here, but we return
+    List<ToDo> list = await database.rawQuery('SELECT * FROM toDoList');
+    return list;
+    // Connection to database should be done here, but we return
     // a sample list instead
-    return [
-      ToDo(name: "Nazwa 1", description: "Opis 1"),
-      ToDo(name: "Nazwa 2", description: "Opis 2"),
-    ];
+    // return [
+    //   ToDo(name: "Nazwa 1", description: "Opis 1"),
+    //   ToDo(name: "Nazwa 2", description: "Opis 2"),
+    // ];
   }
 }

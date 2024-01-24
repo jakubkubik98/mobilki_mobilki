08c66e190c6e1ba12ba18bf4ab931f1d10fd7f06
Jakub 'MrHishprung' Kubik <mail.mrhishprung@gmail.com>
2024-01-24 16:16:13 +0100

    Added database code according to one provided by lecturer

--- main.dart
+++ main.dart
@@ -6,22 +6,31 @@ import 'package:path/path.dart';
 Future<void> main() async {
   var databasePath = await getDatabasesPath();
   String path = join(databasePath, 'database.db');
-  Database database = await openDatabase(path, version: 1,
-    onCreate: (Database db, int (db, version) async {
-      await db.execute('CREATE TABLE ToDoList (id INTEGER PRIMARY KEY, name VARCHAR(255), desc VARCHAR(255)), createdAt TIMESTAMP, isDone BOOL)')
-  }
-  )
-  );
+  Database database = await openDatabase(
+    path, 
+    version: 1,
+    onCreate: (database, version) async {
+      await database.execute('CREATE TABLE toDoList (id INTEGER PRIMARY KEY, name VARCHAR(255), desc VARCHAR(255)), createdAt TIMESTAMP, isDone BOOL)');
+      await database.execute("INSERT INTO toDoList VALUES (1, 'nazwa', 'opis', '1234', '1' )");
+    });
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  Database database;
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InitialScreen(),
+      home: Scaffold(
+        body: FutureBuilder(
+          future: database.rawQuery("SELECT * FROM todos"),
+          builder: (context, snapshot) {
+            return Center(child: Text(snapshot.toString()));
+          },
+        ),
+      ),
     );
   }
 }
